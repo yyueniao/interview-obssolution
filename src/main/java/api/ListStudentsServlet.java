04e31d9c766e2732ba3e667abf7158a808549252
Say Peng <yyueniao0913@gmail.com>
2023-03-26 09:22:58 +0800

    feat(ListStudentsServlet): update list of students
    The `listStudents` function now return as the example of requirement.

--- src/main/java/api/ListStudentsServlet.java
+++ src/main/java/api/ListStudentsServlet.java
@@ -31,9 +31,14 @@ public class ListStudentsServlet extends HttpServlet {
 
   private List<Student> listStudents() {
     List<Student> students = new ArrayList<>();
-    students.add(new Student("123", "John Doe", "Computer Science", 90));
-    students.add(new Student("456", "Jane Smith", "Mathematics", 85));
-    students.add(new Student("789", "Bob Johnson", "Physics", 95));
+    students.add(new Student("S1", "Student 1", "Dep 1", 35));
+    students.add(new Student("S2", "Student 2", "Dep 1", 70));
+    students.add(new Student("S3", "Student 3", "Dep 1", 60));
+    students.add(new Student("S4", "Student 4", "Dep 1", 90));
+    students.add(new Student("S5", "Student 5", "Dep 2", 30));
+    students.add(new Student("S6", "Student 6", "Dep 3", 32));
+    students.add(new Student("S7", "Student 7", "Dep 3", 70));
+    students.add(new Student("S8", "Student 8", "Dep 3", 20));
     return students;
   }
 }
